d3649c93624a815027dad05bc82a000fcba33732
Jacko <jacko1114@hotmail.com>
2021-04-11 00:05:18 +0800

    建置用分層架構的資料夾、安裝Dapper

---
+++ GoodHouse/Models/Entity/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodHouse.Models.Entity
+{
+  public class BaseEntity
+  {
+    public DateTime CreateTime { get; set; }
+    public string CreateUser { get; set; }
+    public DateTime EditeTime { get; set; }
+    public string EditeUser { get; set; }
+  }
+}
